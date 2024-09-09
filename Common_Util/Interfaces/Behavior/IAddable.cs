ac5849bfd8972b496b802d9fb22494c034ae2d23
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-09-09 14:35:06 +0800

    feat: 新增行为接口 IAddable
    可以添加特定的某个类型项的某个东西

---
+++ Common_Util/Interfaces/Behavior/IAddable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Interfaces.Behavior
+{
+    /// <summary>
+    /// 可以添加特定的某个类型项的某个东西
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IAddable<T> 
+    {
+        void Add(T item);
+    }
+    /// <summary>
+    /// 可以添加任意对象的某个东西
+    /// </summary>
+    public interface IAddable : IAddable<object?>
+    {
+
+    }
+}
