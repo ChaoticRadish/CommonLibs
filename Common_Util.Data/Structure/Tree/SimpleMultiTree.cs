bd26f7275aa9bd8e6b7fd7e2300a9208e4d74c45
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-09-09 14:44:35 +0800

    feat: 声明树结构的标记接口 ITree, 新增两个行为树结构的接口, 新增节点可添加子项的多叉树接口, 整理添加与有序插入的扩展方法
    树结构接口: ITree, ITree<TValue>
    行为接口:
    ICanCreateRootNodeFrom<TValue> 可以通过特定值创建根节点的树
    IHasRootNodeAs<TNode> 拥有一个特定类型的根节点的树
    整理多叉树相关扩展方法 (节点值实现 ILayeringAddressCode<TValue> ):
    Add: 现在除了 ObservableMultiTree, IAddableMultiTree 也可以使用这个扩展方法
    OrderlyAdd: 除了 ObservableMultiTree, 其他实现 IHasRootNodeAs<TNode> 的多叉树也可以使用这个扩展方法, 同时如果实现了 ICanCreateRootNodeFrom<TValue> 的多叉树, 在根节点为 null 时, 也可以自动调用来创建根节点

--- Common_Util.Data/Structure/Tree/SimpleMultiTree.cs
+++ Common_Util.Data/Structure/Tree/SimpleMultiTree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,20 @@ namespace Common_Util.Data.Structure.Tree
     /// <summary>
     /// 简易多叉树
     /// </summary>
-    public class SimpleMultiTree<TValue> : IMultiTree<TValue>
+    public class SimpleMultiTree<TValue> 
+        : IAddableMultiTree<TValue>,
+        IHasRootNodeAs<SimpleMultiTreeNode<TValue>>
     {
         public SimpleMultiTreeNode<TValue>? Root { get; set; }
 
         IMultiTreeNode<TValue>? IMultiTree<TValue>.Root => Root;
+        IAddableMultiTreeNode<TValue>? IAddableMultiTree<TValue>.Root => Root;
     }
     /// <summary>
     /// 简易多叉树的节点
     /// </summary>
     /// <typeparam name="TValue"></typeparam>
-    public class SimpleMultiTreeNode<TValue> : IMultiTreeNode<TValue>
+    public class SimpleMultiTreeNode<TValue> : IAddableMultiTreeNode<TValue>
     {
         public SimpleMultiTreeNode(TValue nodeValue) 
         {
@@ -33,5 +37,30 @@ namespace Common_Util.Data.Structure.Tree
         public List<SimpleMultiTreeNode<TValue>>? Childrens { get; set; }
 
         IEnumerable<IMultiTreeNode<TValue>> IMultiTreeNode<TValue>.Childrens => Childrens ?? [];
+        IEnumerable<IAddableMultiTreeNode<TValue>> IAddableMultiTreeNode<TValue>.Childrens => Childrens ?? [];
+
+        #region 操作
+        /// <summary>
+        /// 使用传入值实例化新节点后, 添加到当前节点的子节点集合中
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(TValue item)
+        {
+            TryAdd(item, out _);
+        }
+
+        public bool TryAdd(TValue item, [NotNullWhen(true)] out IAddableMultiTreeNode<TValue>? node)
+        {
+            if (Childrens == null)
+            {
+                Childrens = [];
+            }
+            var _node = new SimpleMultiTreeNode<TValue>(item);
+            Childrens.Add(_node);
+
+            node = _node;
+            return true;
+        }
+        #endregion
     }
 }
