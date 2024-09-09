c30643c318ee9b59890d72e72e790597e34c6cd8
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-09-09 17:06:13 +0800

    feat: 不可能分支异常新增一种构造函数 ImpossibleForkException(string message)
    此构造函数用以在固定的信息后面额外加一些信息方便后续调查

--- Common_Util/Exceptions/General/ImpossibleForkException.cs
+++ Common_Util/Exceptions/General/ImpossibleForkException.cs
@@ -14,6 +14,14 @@ namespace Common_Util.Exceptions.General
     /// </remarks>
     public sealed class ImpossibleForkException : Exception
     {
-        public ImpossibleForkException() : base("代码走到了一个理论上不可能发生的分支! ") { }
+        private const string fixedString = "代码走到了一个理论上不可能发生的分支! ";
+
+        public ImpossibleForkException() : base(fixedString) { }
+
+        /// <summary>
+        /// 实例化时, 附带额外的文本信息, 一般用来描述为什么是不可能发生的分支
+        /// </summary>
+        /// <param name="message"></param>
+        public ImpossibleForkException(string message) : base($"{fixedString}{message}") { }
     }
 }
