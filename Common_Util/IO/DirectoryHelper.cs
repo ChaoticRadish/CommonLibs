647e22250a01ee9fcaaac1d4444a097967fd70ec
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-09-10 01:22:48 +0800

    feat: 新增临时文件接口, 新增临时文件管理器, 分配器(简单的基于需求大小的实现), 临时文件帮助类新增获取默认临时文件管理器的方法
    1. 临时文件接口: 实现 IDisposable, 释放即永久性地丢弃此临时文件, 一般需要通知到对应的管理器
    2. 临时文件管理器: 默认实现, 会在指定的目录里维护临时文件, 该目录下的其余文件会被清理掉, 所以需要设置一个单独的目录给它
    3. 临时文件分配器(简单的基于需求大小的实现): 调用申请分配时, 会根据当前的临时文件已分配大小, 与需求的大小, 决定是要从当前临时文件中划分一个片段, 还是新建临时文件后, 在新文件上划分片段
    4. 临时文件帮助类: 新增获取默认临时文件管理器的方法, 取得的管理器会调用帮助类静态方法得到临时文件, 在释放时也会调用帮助类静态方法释放掉通过该管理器申请的临时文件

---
+++ Common_Util/IO/DirectoryHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.IO
+{
+    public static class DirectoryHelper
+    {
+
+        /// <summary>
+        /// 删除目标文件夹下的所有文件与目录
+        /// </summary>
+        /// <param name="path"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(string path)
+        {
+            Clear(new DirectoryInfo(path));
+        }
+        /// <summary>
+        /// 删除目标文件夹下的所有文件与目录
+        /// </summary>
+        /// <param name="dir"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Clear(DirectoryInfo dir)
+        {
+            if (!dir.Exists)
+            {
+                return;
+            }
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
+            {
+                subDir.Delete(recursive: true);
+            }
+
+        }
+    }
+}
---
+++ Common_Util/Interfaces/IO/ITempFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Interfaces.IO
+{
+    /// <summary>
+    /// 临时文件接口
+    /// </summary>
+    /// <remarks>
+    /// 需实现 <see cref="IDisposable"/>, 以支持使用 <see langword="using"/> 在离开作用域后自动调用 <see cref="IDisposable.Dispose"/> 释放该临时文件 <br/>
+    /// 释放即永久性的丢弃该临时文件
+    /// </remarks>
+    public interface ITempFile : IDisposable
+    {
+        /// <summary>
+        /// 临时文件对应的文件实际绝对路径
+        /// </summary>
+        string Path { get; }
+    }
+    /// <summary>
+    /// 临时文件管理器接口
+    /// </summary>
+    /// <remarks>
+    /// 需实现 <see cref="IDisposable"/>, 以支持使用 <see langword="using"/> 在离开作用域后自动调用 <see cref="IDisposable.Dispose"/> 释放该管理器 <br/>
+    /// 释放即永久性的丢弃该管理器创建的所有临时文件
+    /// </remarks>
+    public interface ITempFileManager : IDisposable
+    {
+        /// <summary>
+        /// 创建并返回一个新的临时文件
+        /// </summary>
+        /// <returns></returns>
+        ITempFile NewOne();
+    }
+    /// <summary>
+    /// 用于创建特定类型临时文件工厂接口
+    /// </summary>
+    public interface ITempFileManager<TTempFile> : ITempFileManager
+        where TTempFile : ITempFile
+    {
+        /// <summary>
+        /// 创建并返回一个新特定类型的临时文件
+        /// </summary>
+        /// <returns></returns>
+        new TTempFile NewOne();
+    }
+}
