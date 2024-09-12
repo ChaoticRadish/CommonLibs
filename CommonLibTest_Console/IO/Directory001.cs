e295d5bf1a4c56db3c08931e11f2fd2e67bf1438
Yi Uta <UtaYiZzzz@outlook.com>
2024-09-13 02:01:27 +0800

    test: 测试 DirectoryHelper 新增的遍历方法

---
+++ CommonLibTest_Console/IO/Directory001.cs
@@ -0,0 +1,27 @@
+﻿using Common_Util.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibTest_Console.IO
+{
+    internal class Directory001() : TestBase("测试文件夹帮助类遍历方法")
+    {
+        protected override void RunImpl()
+        {
+            string strPath = "D:\\笔记";
+
+            foreach (var file in DirectoryHelper.TraversalFiles(strPath, true))
+            {
+                WriteLine(file.FullName);
+            }
+            WriteEmptyLine();
+            foreach (var file in DirectoryHelper.TraversalFiles(strPath, false))
+            {
+                WriteLine(file.FullName);
+            }
+        }
+    }
+}
--- CommonLibTest_Console/Program.cs
+++ CommonLibTest_Console/Program.cs
@@ -19,7 +19,7 @@ namespace CommonLibTest_Console
             AllocConsole();
 #endif
 
-            TestBase test = new DataStruct.Tree003();
+            TestBase test = new IO.Directory001();
             
             test.Run();
             test.Finish();
