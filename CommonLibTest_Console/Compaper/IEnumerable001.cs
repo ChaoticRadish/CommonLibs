cc9c5d247d06af023bcf5bb46bd6a8234cece026
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-09-19 17:43:44 +0800

    test: 测试 IEnumerable<T>.DisorderEquals 无序相等比较方法是否正常执行

---
+++ CommonLibTest_Console/Compaper/IEnumerable001.cs
@@ -0,0 +1,32 @@
+﻿using Common_Util.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibTest_Console.Compaper
+{
+    internal class IEnumerable001() : TestBase("测试 IEnumerable<T>.DisorderEquals 无序相等比较方法")
+    {
+        protected override void RunImpl()
+        {
+            test(["string", "aaa", "bbb"], ["aaa", "string", "bbb", null]);
+            test([null, "string", "aaa", "bbb"], ["aaa", "string", "bbb", null]);
+            test(["string", "aaa", "bbb"], ["aaa", "string", "bbb"]);
+            test(["string", "AAA", "bbb"], ["aaa", "string", "bbb"]);
+            test(["string", "AAA", "bbb"], ["string", "bbb", "AAA"]);
+            test([], []);
+        }
+
+        private void test(IEnumerable<string?> list1, IEnumerable<string?> list2)
+        {
+            WritePair(list1.FullInfoString());
+            WritePair(list2.FullInfoString());
+            WritePair(list1.DisorderEquals(list2));
+            WriteEmptyLine();
+        }
+
+
+    }
+}
--- CommonLibTest_Console/Program.cs
+++ CommonLibTest_Console/Program.cs
@@ -19,7 +19,7 @@ namespace CommonLibTest_Console
             AllocConsole();
 #endif
 
-            TestBase test = new DataWrapper.NeedInitObject001();
+            TestBase test = new Compaper.IEnumerable001();
             
             test.Run();
             test.Finish();
