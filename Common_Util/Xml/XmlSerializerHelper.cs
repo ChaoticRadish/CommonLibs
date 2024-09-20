a3327aa229f882f5b095d7600aa68f9fc7ae6bf1
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-09-21 01:37:38 +0800

    feat: XML 格式序列化帮助类各方法新增参数 encoding
    用于指定需要使用的字符集, 如果传入 null, 均默认采用 UTF-8 字符集

--- Common_Util/Xml/XmlSerializerHelper.cs
+++ Common_Util/Xml/XmlSerializerHelper.cs
@@ -15,26 +15,35 @@ namespace Common_Util.Xml
         /// <summary>
         /// 将输入对象以 XML 格式序列化为二进制数据
         /// </summary>
+        /// <remarks>
+        /// 此方法通过实例化 <see cref="MemoryStream"/> 后调用 <see cref="WriteToStreamAsXml{T}(T, Stream, Encoding?)"/> 实现
+        /// </remarks>
         /// <typeparam name="T"></typeparam>
         /// <param name="data"></param>
+        /// <param name="encoding">字符集, 如果为 <see langword="null"/>, 则采用 <see cref="Encoding.UTF8"/></param>
         /// <returns></returns>
-        public static byte[] ToBinaryAsXml<T>(T data)
+        public static byte[] ToBinaryAsXml<T>(T data, Encoding? encoding = null)
         {
             using MemoryStream memoryStream = new();
-            WriteToStreamAsXml(data, memoryStream);
+            WriteToStreamAsXml(data, memoryStream, encoding ?? Encoding.UTF8);
             return memoryStream.ToArray();
         }
         /// <summary>
         /// 将输入对象以 XML 格式序列化后写入传入的流
         /// </summary>
+        /// <remarks>
+        /// 此方法基于 <see cref="XmlSerializer.Serialize(TextWriter, object?)"/> 方法实现
+        /// </remarks>
         /// <typeparam name="T"></typeparam>
         /// <param name="data"></param>
         /// <param name="stream"></param>
-        public static void WriteToStreamAsXml<T>(T data, Stream stream)
+        /// <param name="encoding">字符集, 如果为 <see langword="null"/>, 则采用 <see cref="Encoding.UTF8"/></param>
+        public static void WriteToStreamAsXml<T>(T data, Stream stream, Encoding? encoding = null)
         {
             stream.Seek(0, SeekOrigin.Begin);
             XmlSerializer serializer = new(typeof(T));
-            serializer.Serialize(stream, data);
+            TextWriter writer = new StreamWriter(stream, encoding ?? Encoding.UTF8);
+            serializer.Serialize(writer, data);
         }
 
         /// <summary>
@@ -51,15 +60,20 @@ namespace Common_Util.Xml
         /// <summary>
         /// 将数据流以 XML 格式反序列化为指定类型的对象
         /// </summary>
+        /// <remarks>
+        /// 此方法基于 <see cref="XmlSerializer.Deserialize(TextReader)"/> 方法实现
+        /// </remarks>
         /// <typeparam name="T"></typeparam>
         /// <param name="stream"></param>
+        /// <param name="encoding">字符集, 如果为 <see langword="null"/>, 则采用 <see cref="Encoding.UTF8"/></param>
         /// <returns></returns>
         /// <exception cref="InvalidOperationException"></exception>
-        public static T InitByStreamAsXml<T>(Stream stream)
+        public static T InitByStreamAsXml<T>(Stream stream, Encoding? encoding = null)
         {
             stream.Seek(0, SeekOrigin.Begin);
             XmlSerializer serializer = new(typeof(T));
-            return (T)(serializer.Deserialize(stream) ?? throw new InvalidOperationException($"未能将传入数据以 XML 格式反序列化为类型 {typeof(T)}"));
+            TextReader reader = new StreamReader(stream, encoding ?? Encoding.UTF8);
+            return (T)(serializer.Deserialize(reader) ?? throw new InvalidOperationException($"未能将传入数据以 XML 格式反序列化为类型 {typeof(T)}"));
         }
     }
 }
