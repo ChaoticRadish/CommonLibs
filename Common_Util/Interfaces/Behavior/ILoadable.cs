42a2d3c254dc577ed9dc0846a390e912020d3e1e
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-09-22 19:04:48 +0800

    feat: 新增行为接口 ILoadable, 新增基于引用计数的加卸载器 RefCountedLoader 及用于返回数据引用的类型 LoadedReference<TData>
    - ILoadble:
    一个可以执行加卸载的东西
    - RefCountedLoader<TLoadable, TData>:
    有任意引用时就加载数据, 没有引用时就卸载数据的一个加卸载器, 通过其中的 Obtain 方法可以获取加载后数据的一个引用, 需调用引用对象的 Dispose 方法释放引用
    - LoadedReference<TData>:
    加载所取得数据的引用, 其中 Data 只允许初始时设置一次非空值, 根据创建者需求, 需配置不同的释放方法, 比如 RefCountedLoader 创建的就需要减去一个引用计数

---
+++ Common_Util/Interfaces/Behavior/ILoadable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Interfaces.Behavior
+{
+    /// <summary>
+    /// 具有加卸载行为的某个东西
+    /// </summary>
+    public interface ILoadable
+    {
+        /// <summary>
+        /// 加载
+        /// </summary>
+        void Load();
+        /// <summary>
+        /// 卸载
+        /// </summary>
+        void Unload();
+    }
+}
