d213920e7999431d940efe3f91c2fe7b7e6407f7
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-09-22 19:04:48 +0800

    feat: 新增行为接口 ILoadable, 新增基于引用计数的加卸载器 RefCountedLoader 及用于返回数据引用的类型 LoadedReference<TData>
    - ILoadble:
    一个可以执行加卸载的东西
    - RefCountedLoader<TLoadable, TData>:
    有任意引用时就加载数据, 没有引用时就卸载数据的一个加卸载器, 通过其中的 Obtain 方法可以获取加载后数据的一个引用, 需调用引用对象的 Dispose 方法释放引用
    - LoadedReference<TData>:
    加载所取得数据的引用, 其中 Data 只允许初始时设置一次非空值, 根据创建者需求, 需配置不同的释放方法, 比如 RefCountedLoader 创建的就需要减去一个引用计数

---
+++ Common_Util/Module/Loadable/LoadedReference.cs
@@ -0,0 +1,50 @@
+﻿using Common_Util.Data.Struct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Module.Loadable
+{
+    /// <summary>
+    /// 经过加载取得数据的引用对象
+    /// </summary>
+    /// <typeparam name="TData">加载取得的数据类型</typeparam>
+    public sealed class LoadedReference<TData> : IDisposable
+    {
+        public LoadedReference()
+        {
+
+        }
+        /// <summary>
+        /// 实例化一个包装了另一个可释放对象的实例, 在释放此对象时, 会调用 <paramref name="beWrapper"/> 的 <see cref="IDisposable.Dispose"/> 方法
+        /// </summary>
+        /// <param name="beWrapper"></param>
+        public LoadedReference(IDisposable beWrapper)
+        {
+            this.beWrapper = beWrapper;
+        }
+
+        /// <summary>
+        /// 加载取得的数据
+        /// </summary>
+        public TData Data { get => initWrapper.Value; set => initWrapper.Value = value; }
+        private NeedInitObjectImmutable<TData> initWrapper;
+
+
+
+        /// <summary>
+        /// 释放时需要执行的内容
+        /// </summary>
+        public required Action OnDisposing { get; set; }
+
+        private readonly IDisposable? beWrapper;
+
+        public void Dispose()
+        {
+            OnDisposing.Invoke();
+            beWrapper?.Dispose();
+        }
+    }
+}
