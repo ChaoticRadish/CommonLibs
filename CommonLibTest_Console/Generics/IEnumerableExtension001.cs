d5971a04ae80f5a6b4da3525891d7dbd1aeea3e8
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-12-15 18:48:56 +0800

    test: 测试 IEnumerableExtensions.UntilAllAwayWithIndex

--- CommonLibTest_Console/Generics/IEnumerableExtension001.cs
+++ CommonLibTest_Console/Generics/IEnumerableExtension001.cs
@@ -11,12 +11,25 @@ namespace CommonLibTest_Console.Generics
     {
         protected override void RunImpl()
         {
-            test(["111", null, "222"], ["333", null]);
-            test([123, 32, 1], [1, 2, 3, 4, 5]);
-            test<int?>([123, 32, 1], [1, 2, 3, 4, 5]);
+            RunTestMark();
         }
 
-        void test<T>(T[] tArr1, T[] tArr2)
+        [TestMethod]
+        void test1s()
+        {
+            test1(["111", null, "222"], ["333", null]);
+            test1([123, 32, 1], [1, 2, 3, 4, 5]);
+            test1<int?>([123, 32, 1], [1, 2, 3, 4, 5]);
+        }
+        [TestMethod]
+        void test2s()
+        {
+            test2(["111", null, "222"], ["333", null]);
+            test2([123, 32, 1], [1, 2, 3, 4, 5]);
+            test2<int?>([123, 32, 1], [1, 2, 3, 4, 5]);
+        }
+
+        void test1<T>(T[] tArr1, T[] tArr2)
         {
             WriteEmptyLine();
             int index = 0;
@@ -27,6 +40,18 @@ namespace CommonLibTest_Console.Generics
             }
             WriteEmptyLine();
         }
-        
+        void test2<T>(T[] tArr1, T[] tArr2)
+        {
+            WriteEmptyLine();
+            int index = 0;
+            // foreach (((int i1, T? t1), (int i2, T? t2)) in (tArr1, tArr2).UntilAllAwayWithIndex())
+            foreach (var ((i1, t1), (i2, t2)) in (tArr1, tArr2).UntilAllAwayWithIndex())
+            {
+                WriteLine($"{index}  =>  t1: [{i1}]{t1?.ToString() ?? "<null>"} ::: t2: [{i2}]{t2?.ToString() ?? "<null>"}");
+                index++;
+            }
+            WriteEmptyLine();
+        }
+
     }
 }
