f909d28d69e6f89b8f0fca2817dc8e20983dc94b
ChaoticRadish <UtaYiZzzz@outlook.com>
2024-12-15 23:12:40 +0800

    feat: IEnumerableExtensions 完善 DisorderEquals 方法, 新增 ExcludeDisorderEquals 方法
    DisorderEquals: 完善注释, 优化实现
    ExcludeDisorderEquals: 传入两个可遍历对象, 返回分别遍历这两个对象无法一一对应的部分的两个新的可遍历对象

--- Common_Util/Extensions/IEnumerableExtensions.cs
+++ Common_Util/Extensions/IEnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common_Util.Data.Struct;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,11 +30,14 @@ namespace Common_Util.Extensions
             }
         }
 
+        #region 无序比较
         /// <summary>
         /// 比较两个集合内的元素能否一一对应. 此比较与顺序无关, 类似于连连看.
         /// </summary>
         /// <remarks>
-        /// 当前实现会将集合转换为数组来实现, 数据量较大时可能会有性能问题
+        /// 该实现会采用 <paramref name="equalityComparer"/> 的 <see cref="IEqualityComparer{T}.GetHashCode(T)"/> 方法获取哈希码, 而不是通过 <see cref="object.GetHashCode"/> 方法来获取 <br/>
+        /// 当 <paramref name="thisOne"/> 和 <paramref name="otherOne"/> 拥有元素值相等但是数量不相等的元素时, 将 <br/>
+        /// 注: 该实现会将集合转换为数组来实现, 数据量较大时可能会有性能问题! <br/>
         /// </remarks>
         /// <typeparam name="T"></typeparam>
         /// <param name="thisOne"></param>
@@ -49,14 +53,13 @@ namespace Common_Util.Extensions
 
             equalityComparer ??= EqualityComparer<T>.Default;
 
-
-            Dictionary<int, List<T>> group1 = [];
+            SortedDictionary<int, List<T>> group1 = [];
             int arr1NullCount = 0;
 
-            Dictionary<int, List<T>> group2 = [];
+            SortedDictionary<int, List<T>> group2 = [];
             int arr2NullCount = 0;
 
-            static void tidyHashCode(T[] arr, Dictionary<int, List<T>> groupContainer, ref int count, IEqualityComparer<T> equalityComparer)
+            static void tidyHashCode(T[] arr, SortedDictionary<int, List<T>> groupContainer, ref int count, IEqualityComparer<T> equalityComparer)
             {
                 foreach (T? item in arr)
                 {
@@ -80,57 +83,180 @@ namespace Common_Util.Extensions
 
             if (arr1NullCount != arr2NullCount) return false;
             if (group1.Count != group2.Count) return false;
+            foreach (var (kvp1, kvp2) in (group1, group2).UntilAnyAway())   // 两者数量相等, 会同时结束
+            {
+                if (kvp1.Key != kvp2.Key) return false;                 // 顺序排列的字典, 相同位置元素的哈希码不等时, 肯定会出现无法对应上的情况
+                if (kvp1.Value.Count != kvp2.Value.Count) return false; // 同时, 相同哈希码的元素数量不等时, 也会出现无法对应上的情况
+            }
 
-            List<int> ignoreIndexMark = []; // 比较两组数据中相同 HashCode 的项的过程中, 标记后者中已有对应关系的项
+            List<int> ignoreIndexMark = []; // 比较两组数据中相同哈希码的项的过程中, 标记后者中已有对应关系的项
             foreach (int hashCode in group1.Keys)
             {
                 var list1 = group1[hashCode];
-                if (group2.TryGetValue(hashCode, out var list2))
+                var list2 = group2[hashCode];
+
+                if (list1.Count == 1)
                 {
-                    if (list1.Count != list2.Count)
+                    if (equalityComparer.Equals(list1[0], list2[0]))
                     {
-                        // 同一个 HashCode, 两者拥有数量不同
+                        break;
+                    }
+                    else
+                    {
                         return false;
                     }
-                    if (list1.Count == 1)
+                }
+
+                // 两组数据均存在多个相同 HashCode 的项, 比较两组分别拥有的能否一一对应消除
+                ignoreIndexMark.Clear();
+
+                foreach (T item1 in list1)
+                {
+                    bool foundFlag = false;
+                    for (int index = 0; index < list2.Count; index++)
                     {
-                        if (equalityComparer.Equals(list1[0], list2[0]))
+                        if (ignoreIndexMark.Contains(index)) continue;  // 忽略已有对应关系的项
+
+                        T item2 = list2[index];
+                        if (equalityComparer.Equals(item1, item2))
                         {
+                            ignoreIndexMark.Add(index);
+                            foundFlag = true;
                             break;
+                        }
+                    }
+                    if (!foundFlag) return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 比较并排除两个集合内能够一一对应的元素, 最终返回两组元素中相斥的部分. 此比较与顺序无关, 类似于连连看.
+        /// </summary>
+        /// <remarks>
+        /// 该实现会采用 <paramref name="equalityComparer"/> 的 <see cref="IEqualityComparer{T}.GetHashCode(T)"/> 方法获取哈希码, 而不是通过 <see cref="object.GetHashCode"/> 方法来获取 <br/>
+        /// 当两个集合中有相等的元素 A, <paramref name="thisOne"/> 有 2 个, <paramref name="otherOne"/> 有 1 个, 此时排除一一对应元素后, <paramref name="thisOne"/> 还会剩余 1 个, 而 <paramref name="otherOne"/> 就没有了 <br/>
+        /// 注: 该实现会将集合转换为数组来实现, 数据量较大时可能会有性能问题! <br/>
+        /// </remarks>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="thisOne"></param>
+        /// <param name="otherOne"></param>
+        /// <param name="equalityComparer"></param>
+        /// <returns></returns>
+        public static (IEnumerable<T> ThisOneExclusion, IEnumerable<T> OtherOneExclusion) ExcludeDisorderEquals<T>(
+            this IEnumerable<T> thisOne, IEnumerable<T> otherOne, 
+            IEqualityComparer<T>? equalityComparer = null)
+        {
+            var arr1 = thisOne.ToArray();
+            var arr2 = otherOne.ToArray();
+
+            // 任意一方的长度为 0 时, 不用作排除操作
+            if (arr1.Length == 0 || arr2.Length == 0)
+            {
+                return (arr1, arr2);
+            }
+
+            equalityComparer ??= EqualityComparer<T>.Default;
+
+            // null 值元素
+            IndexMask nullIndexMask1 = new(arr1.Length, false);
+            int nullCount1 = 0;
+            IndexMask nullIndexMask2 = new(arr2.Length, false);
+            int nullCount2 = 0;
+
+            SortedDictionary<int, List<(T item, int index)>> group1 = [];
+            SortedDictionary<int, List<(T item, int index)>> group2 = [];
+            static void tidyHashCode(T[] arr, IndexMask nullIndexMaskArr, SortedDictionary<int, List<(T item, int index)>> groupContainer, ref int nullCount, IEqualityComparer<T> equalityComparer)
+            {
+                int index = 0;
+                foreach (T? item in arr)
+                {
+                    if (item is null) 
+                    {
+                        nullIndexMaskArr[index] = true;
+                        nullCount++;
+                    }
+                    else
+                    {
+                        int hashCode = equalityComparer.GetHashCode(item);
+                        if (groupContainer.TryGetOrAdd(hashCode, static () => [], out var list))
+                        {
+                            list.Add((item, index));
                         }
                         else
                         {
-                            return false;
+                            throw new InvalidOperationException($"未能添加新项 (hashCode: {hashCode}) 到字典中");
                         }
                     }
-
-                    // 两组数据均存在多个相同 HashCode 的项, 比较两组分别拥有的能否一一对应消除
-                    ignoreIndexMark.Clear();
-
-                    foreach (T item1 in list1)
-                    {
-                        bool foundFlag = false;
-                        for (int index = 0; index < list2.Count; index++)
-                        {
-                            if (ignoreIndexMark.Contains(index)) continue;  // 忽略已有对应关系的项
-
-                            T item2 = list2[index];
-                            if (equalityComparer.Equals(item1, item2))
-                            {
-                                ignoreIndexMark.Add(index);
-                                foundFlag = true;
-                                break;
-                            }
-                        }
-                        if (!foundFlag) return false;
-                    }
-                }
-                else
-                {
-                    // 存在任意 HashCode, 只有前者拥有, 后者没有
-                    return false;
+                    index++;
                 }
             }
+
+            tidyHashCode(arr1, nullIndexMask1, group1, ref nullCount1, equalityComparer);
+            tidyHashCode(arr2, nullIndexMask2, group2, ref nullCount2, equalityComparer);
+
+            // 已被排除项
+            IndexMask excluedIndexMask1;
+            IndexMask excluedIndexMask2;
+            // 初始排除掉能够匹配上的 null 值
+            if (nullCount1 == nullCount2)
+            {
+                excluedIndexMask1 = nullIndexMask1;
+                excluedIndexMask2 = nullIndexMask2;
+            }
+            else
+            {
+                int minNullCount = Math.Min(nullCount1, nullCount2);
+                int counter1 = 0;
+                int counter2 = 0;
+                excluedIndexMask1 = new(nullIndexMask1.All().Select(i =>
+                {
+                    if (!i) return false;
+                    else return counter1++ < minNullCount;
+                }), arr1.Length);
+                excluedIndexMask2 = new(nullIndexMask2.All().Select(i =>
+                {
+                    if (!i) return false;
+                    else return counter2++ < minNullCount;
+                }), arr2.Length);
+            }
+
+            foreach (var (kvp1, kvp2) in group1.Join(group2, g1 => g1.Key, g2 => g2.Key, (k1, k2) => (k1, k2)))
+            {
+                // true => 已被消除
+                IndexMask kvp1IndexMask = new(kvp1.Value.Count, false);
+                IndexMask kvp2IndexMask = new(kvp2.Value.Count, false);
+
+                foreach (var (i1, v1) in kvp1.Value.WithIndex())
+                {
+                    int foundInSub2 = -1;
+                    int foundInInput2 = -1;
+                    foreach (var (i2, v2) in kvp2.Value.WithIndex().Filtering(kvp2IndexMask))
+                    {
+                        if (equalityComparer.Equals(v1.item, v2.item))
+                        {
+                            foundInSub2 = i2;
+                            foundInInput2 = v2.index;
+                            break;
+                        }
+                    }
+                    if (foundInSub2 >= 0)
+                    {
+                        // 标记已匹配上
+                        kvp1IndexMask[i1] = true;
+                        kvp2IndexMask[foundInSub2] = true;
+                        excluedIndexMask1[v1.index] = true;
+                        excluedIndexMask2[foundInInput2] = true;
+                    }
+                }
+            }
+
+            return (arr1.Filtering(excluedIndexMask1), arr2.Filtering(excluedIndexMask2));
+        }
+        #endregion
+
 
         #region 遍历
         /// <summary>
