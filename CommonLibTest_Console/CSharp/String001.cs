104e8c55a995a0ea66c6af59deeb72d085c33950
Yi Uta <UtaYiZzzz@outlook.com>
2024-12-19 17:43:45 +0800

    test: 测试值为 0x00 的字符直接转换为字符串的使用

---
+++ CommonLibTest_Console/CSharp/String001.cs
@@ -0,0 +1,20 @@
+﻿using Common_Util.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibTest_Console.CSharp
+{
+    internal class String001() : TestBase("字符串与 0x00 字符")
+    {
+        protected override void RunImpl()
+        {
+            string str = ((char)0).ToString();
+            WritePair(str.Length);
+            WritePair(str.FullInfoString());
+            WritePair(str.Select(i => (byte)i).ToHexString());
+        }
+    }
+}
