041c5704563983678777120e22ef6034a6ec79a0
ChaoticRadish <UtaYiZzzz@outlook.com>
2025-01-19 01:04:13 +0800

    test: 对 Type 的扩展方法 CanBeNull 的测试

---
+++ CommonLibTest_Console/CSharp/Reflection008.cs
@@ -0,0 +1,31 @@
+﻿using Common_Util.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibTest_Console.CSharp
+{
+    internal class Reflection008() : TestBase("可空类型的判断")
+    {
+        protected override void RunImpl()
+        {
+            WritePair(typeof(string).CanBeNull());
+            WritePair(typeof(int?).CanBeNull());
+            WritePair(typeof(int).CanBeNull());
+            WritePair(typeof(MyStruct).CanBeNull());
+            WritePair(typeof(MyInterface).CanBeNull());
+            WritePair(typeof(Reflection008).CanBeNull());
+        }
+
+        struct MyStruct
+        {
+
+        }
+        interface MyInterface
+        {
+
+        }
+    }
+}
