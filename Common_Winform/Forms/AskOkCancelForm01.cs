cc66ebc35d4c729631d1dc3486c8f1294256efcd
Yi Uta <UtaYiZzzz@outlook.com>
2025-01-21 21:51:22 +0800

    feat: 完善 AskOkCancelForm01 的注释, 新增 OkCancelForm01 窗口
    OkCancelForm01 可以在窗口上半部分显示传入的控件

--- Common_Winform/Forms/AskOkCancelForm01.cs
+++ Common_Winform/Forms/AskOkCancelForm01.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace Common_Winform.Forms
 {
+    /// <summary>
+    /// 询问 "确定" 或 "取消" 的窗口. 显示内容为设定的文本
+    /// </summary>
     public partial class AskOkCancelForm01 : Form, IAskOkCancelForm
     {
         public AskOkCancelForm01()
@@ -20,5 +23,6 @@ namespace Common_Winform.Forms
         public string Title { get => Text; set => Text = value; }
 
         public string ShowingText { get => Shower_主要信息.Text; set => Shower_主要信息.Text = value; }
+
     }
 }
