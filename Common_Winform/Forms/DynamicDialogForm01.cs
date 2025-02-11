75f20a3d7e0c39d323b51c864d9fa95bdff7abaa
ChaoticRadish <UtaYiZzzz@outlook.com>
2025-02-11 23:22:51 +0800

    feat: Common_Winform 调整控件, 新增用于弹窗的窗口类型, 新增与调整了一些扩展方法
    -- 下拉控件 (基类, 列表, 以及枚举值的):
    调整设置可选项的方法改为公开
    -- 分页控件:
    修正切换页码时不更新显示的页码的问题
    -- 扩展方法:
    a. 新增 ContextMenuStrip 扩展方法:
      1. 获取所有子项
      2. 获取所有指定类型的子项
    b. 新增 Control 扩展方法:
      1. 尝试在合适的线程执行传入带返回值的方法, 返回一个操作结果 OperationResultEx<T>
      2. 取得当前 Dpi 缩放比例的方法 ( 当前Dpi / 固定值96f )
      3. 获取数据, 然后根据结果执行响应的方法. 新增的这个方法相当于把一个常用的操作封装了一下
    c. DataGridView 扩展方法:
      1. SetRightButtonMenu 方法, 现在在找不到窗口的情况下, 会在屏幕上, 鼠标的当前位置处显示菜单
      2. 新增: 根据当前分辨率 (dpi) 的比例百分比, 同比在当前基础上, 修改列宽和行高的方法
    -- 用于弹窗的窗口类型
    a. 询问确定或取消的窗口, 完善注释
    b. 新增窗口 DynamicDialogForm01, 使用时需要设置主体控件和需要使用哪些按钮
    c. 新增窗口 OkCancelForm01, 可以设置主体的控件 (而非只有文字)

---
+++ Common_Winform/Forms/DynamicDialogForm01.cs
@@ -0,0 +1,181 @@
+﻿using Common_Util.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Common_Winform.Forms
+{
+    public partial class DynamicDialogForm01 : Form
+    {
+        public DynamicDialogForm01()
+        {
+            InitializeComponent();
+
+            更新按钮列表();
+
+            ClearBody();
+            SetDefault();
+        }
+
+        #region 按钮列表
+
+
+        private Dictionary<DialogResult, Button> 已创建按钮列表 = [];
+        private void 更新按钮列表()
+        {
+            按钮流式布局容器.Controls.Clear();
+            已创建按钮列表.Invoke(i => i.Value.Visible = false).Finish();
+
+            var list = NeedResults.OrderByDescending(i => i);
+            foreach (var item in list)
+            {
+                if (已创建按钮列表.TryGetOrAdd(item, () => 创建按钮(item), out Button? button))
+                {
+                    button.Visible = true;
+                    按钮流式布局容器.Controls.Add(button);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"未能创建或从字典中获取已有按钮, 对应值: {item}");
+                }
+            }
+        }
+        private Button 创建按钮(DialogResult result)
+        {
+            Button button = new Button()
+            {
+                Size = new Size(100, 30),
+                DialogResult = result,
+                Text = 按钮文本映射.GetValueOrDefault(result, result.ToString()),
+            };
+            return button;
+        }
+        private static Dictionary<DialogResult, string> 按钮文本映射 = new()
+        {
+            { DialogResult.None, "无" },
+            { DialogResult.OK, "确认" },
+            { DialogResult.Cancel, "取消" },
+            { DialogResult.Abort, "中止" },
+            { DialogResult.Retry, "重试" },
+            { DialogResult.Ignore, "忽略" },
+            { DialogResult.Yes, "是" },
+            { DialogResult.No, "否" },
+            { DialogResult.TryAgain, "再试一次" },
+            { DialogResult.Continue, "继续" },
+        };
+
+        public DialogResult[] NeedResults
+        {
+            get => dialogResults;
+            set
+            {
+                dialogResults = value ?? [];
+                更新按钮列表();
+            }
+        }
+        private DialogResult[] dialogResults = [ DialogResult.OK ];
+
+
+        #endregion
+
+        #region 主体区域
+
+        /// <summary>
+        /// 窗口标题
+        /// </summary>
+        public string Title
+        {
+            get => base.Text;
+            set => base.Text = value;
+        }
+
+        /// <summary>
+        /// 默认显示内容文本
+        /// </summary>
+        public string DefaultText
+        {
+            get => 默认显示内容.Text;
+            set
+            {
+                默认显示内容.Text = value;
+            }
+        }
+
+        /// <summary>
+        /// 设置控件为窗口的主体
+        /// </summary>
+        public Control? Body
+        {
+            get => body;
+            set
+            {
+                body = value;
+                if (body == null)
+                {
+                    ClearBody();
+                    SetDefault();
+                }
+                else
+                {
+                    ClearBody();
+                    SetBody(body);
+                }
+            }
+        }
+        private Control? body;
+        private void ClearBody()
+        {
+            显示容器.Controls.Clear();
+        }
+        private void SetDefault()
+        {
+            显示容器.Controls.Add(默认显示内容);
+        }
+        private void SetBody(Control body)
+        {
+            显示容器.Controls.Add(body);
+            // body.Dock = DockStyle.None;
+            // body.Anchor = AnchorStyles.Left | AnchorStyles.Top;
+            body.Visible = false;
+            body.Show();
+        }
+
+        #endregion
+
+        #region 静态方法, 常用预设
+        /// <summary>
+        /// 创建一个多行文本的消息提示框, 只有一个确认按钮
+        /// </summary>
+        /// <returns></returns>
+        public static (DynamicDialogForm01 form, Action<string> setTextAction) MessageBox()
+        {
+            RichTextBox textBox = new RichTextBox()
+            {
+                Dock = DockStyle.Fill,
+                ReadOnly = true,
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+            DynamicDialogForm01 form = new DynamicDialogForm01()
+            {
+                NeedResults = [DialogResult.OK],
+                Body = textBox,
+                StartPosition = FormStartPosition.CenterParent,
+            };
+            form.Shown += (o, e) =>
+            {
+                form.BringToFront();
+            };
+
+            return (form, (str) => textBox.Text = str);
+        }
+        #endregion
+
+    }
+
+}
---
+++ Common_Winform/Forms/OkCancelForm01.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Common_Winform.Forms
+{
+    partial class OkCancelForm01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            Button_Cancel = new Button();
+            Button_Ok = new Button();
+            显示容器 = new Panel();
+            默认显示内容 = new Label();
+            调整尺寸的计时器 = new System.Windows.Forms.Timer(components);
+            tableLayoutPanel1.SuspendLayout();
+            显示容器.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 6;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 27.7777786F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 27.7777786F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableLayoutPanel1.Controls.Add(Button_Cancel, 4, 1);
+            tableLayoutPanel1.Controls.Add(Button_Ok, 1, 1);
+            tableLayoutPanel1.Controls.Add(显示容器, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.Size = new Size(588, 267);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // Button_Cancel
+            // 
+            Button_Cancel.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            Button_Cancel.DialogResult = DialogResult.Cancel;
+            Button_Cancel.Font = new Font("Microsoft YaHei UI", 11F);
+            Button_Cancel.Location = new Point(370, 225);
+            Button_Cancel.Margin = new Padding(12, 3, 12, 3);
+            Button_Cancel.Name = "Button_Cancel";
+            Button_Cancel.Size = new Size(139, 30);
+            Button_Cancel.TabIndex = 2;
+            Button_Cancel.Text = "取消";
+            Button_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // Button_Ok
+            // 
+            Button_Ok.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            Button_Ok.DialogResult = DialogResult.OK;
+            Button_Ok.Font = new Font("Microsoft YaHei UI", 11F);
+            Button_Ok.Location = new Point(77, 225);
+            Button_Ok.Margin = new Padding(12, 3, 12, 3);
+            Button_Ok.Name = "Button_Ok";
+            Button_Ok.Size = new Size(139, 30);
+            Button_Ok.TabIndex = 1;
+            Button_Ok.Text = "确定";
+            Button_Ok.UseVisualStyleBackColor = true;
+            // 
+            // 显示容器
+            // 
+            tableLayoutPanel1.SetColumnSpan(显示容器, 6);
+            显示容器.Controls.Add(默认显示内容);
+            显示容器.Location = new Point(10, 10);
+            显示容器.Margin = new Padding(10);
+            显示容器.Name = "显示容器";
+            显示容器.Size = new Size(568, 193);
+            显示容器.TabIndex = 3;
+            // 
+            // 默认显示内容
+            // 
+            默认显示内容.Dock = DockStyle.Fill;
+            默认显示内容.Location = new Point(0, 0);
+            默认显示内容.Name = "默认显示内容";
+            默认显示内容.Size = new Size(568, 193);
+            默认显示内容.TabIndex = 0;
+            默认显示内容.Text = "默认显示内容";
+            默认显示内容.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // OkCancelForm01
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(588, 267);
+            Controls.Add(tableLayoutPanel1);
+            Name = "OkCancelForm01";
+            Text = "OkCancelForm01";
+            tableLayoutPanel1.ResumeLayout(false);
+            显示容器.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button Button_Cancel;
+        private Button Button_Ok;
+        private Panel 显示容器;
+        private Label 默认显示内容;
+        private System.Windows.Forms.Timer 调整尺寸的计时器;
+    }
+}
