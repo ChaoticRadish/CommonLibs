dc21dbd8e6ae39c97ccb0886fa88bc725b0fe353
ChaoticRadish <UtaYiZzzz@outlook.com>
2025-05-25 22:21:44 +0800

    feat: 合并开发分支
    新增:
    1. 比较属性值是否相等的比较器
    2. 创建 Guid 的帮助类 GuidHelper
    3. 文本类型枚举值及扩展方法 TextTypeEnum
    4. 将文件流读取为 IEnumerable<char> 的方法以及扩展方法
    优化:
    1. 队列处理输入日志信息的日志输出器

---
+++ Common_Util/Extensions/StreamExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Extensions
+{
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// 将 <paramref name="stream"/> 按 <paramref name="encoding"/> 字符集读取为字符集合
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="encoding">字符集, 如果是 <see langword="null"/>, 则使用 <see cref="Encoding.UTF8"/> </param>
+        /// <returns></returns>
+        public static IEnumerable<char> AsCharEnumerable(this Stream stream, Encoding? encoding = null)
+        {
+            return String.StringHelper.AsEnumerable(stream, encoding);
+        }
+    }
+}
