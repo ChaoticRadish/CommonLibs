684421b18e9329700dda78a3b942c97b713bc7af
ChaoticRadish <UtaYiZzzz@outlook.com>
2025-07-28 00:33:30 +0800

    feat: 重写配置模块代码的部分内容, 调整相关的一点内容
    配置模块:
    1. 将 ConfigHelper 中主要的代码, 抽离为 IConfigManger 与默认实现 ConfigManager
    2. 配置读写实现的接口调整为任意类型的键值, 而不是固定由类型作为键值
    3. 新增扩展性质的读写管理接口与读写实现接口, 如 IKeyedConfigReadWriteImpl<TKey>, ICachedConfigManager<TKey> 等
    4. 新增结构 StringCachedConfigManagerWrapper<TCompatibleKey> 和 StringKeyedConfigReadWriteImplWrapper<TCompatibleKey>, 用于新的 ConfigHelper 中, 对不同配置管理器类型的适配
    其他:
    1. 新增类型 WorkCountGuard 工作计数守卫, 用于等待所有工作完成后暂时阻止新工作开始, 执行一些内容后再恢复工作
    2. 调整 LevelLoggerHelper 的 LogToLoggerConfig, 用于 LogTo 方法对日志主要文本部分做一些处理
    3. 对 MaybeNull<T> 结构体做了一些优化

---
+++ Common_Util/Module/Config/ConfigManager.cs
@@ -0,0 +1,334 @@
+﻿using Common_Util.Data.Struct;
+using Common_Util.Extensions;
+using Common_Util.Module.Concurrency;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Module.Config
+{
+    /// <summary>
+    /// 配置管理器的默认实现
+    /// </summary>
+    public class ConfigManager : ITypeCachedConfigManager
+    {
+        public IReadOnlyDictionary<string, IKeyedConfigReadWriteImpl<Type>> Impls => impls;
+        private readonly ConcurrentDictionary<string, IKeyedConfigReadWriteImpl<Type>> impls = [];
+
+        public IKeyedConfigReadWriteImpl<Type> DefaultImpl 
+        { 
+            get => settedDefaultImpl ?? defaultImpl;
+            set => settedDefaultImpl = value;
+        }
+        private IKeyedConfigReadWriteImpl<Type>? settedDefaultImpl;
+        private readonly IKeyedConfigReadWriteImpl<Type> defaultImpl = new ConfigurationManagerReadWriteImpl();
+
+        IConfigReadWriteImpl IConfigManager.DefaultImpl => DefaultImpl;
+
+        IReadOnlyDictionary<string, IConfigReadWriteImpl> IConfigManager.Impls => impls.ToDictionary(kvp => kvp.Key, kvp => (IConfigReadWriteImpl)kvp.Value);
+
+        public bool IsAvailable(IConfigReadWriteImpl impl)
+        {
+            if (impl == null) return false;
+            else
+            {
+                return impl.GetType().IsAssignableTo(typeof(IKeyedConfigReadWriteImpl<Type>));
+            }
+        }
+        public void SetDefaultImpl<TImpl>(TImpl impl) where TImpl : IConfigReadWriteImpl
+        {
+            DefaultImpl = (IKeyedConfigReadWriteImpl<Type>)impl;
+        }
+
+        public bool HasImpl(string name)
+        {
+            name ??= string.Empty;
+            return impls.ContainsKey(name);
+        }
+
+        #region 基于 ConcurrentDictionary<string, IKeyedConfigReadWriteImpl<Type>> 实现操作方法
+
+        /// <summary>
+        /// 添加与更新操作的守卫对象
+        /// </summary>
+        /// <remarks>
+        /// 执行这两个操作时不允许执行比较移除 (<see cref="TryRemoveImpl(string, MaybeNull{IKeyedConfigReadWriteImpl{Type}}, out IKeyedConfigReadWriteImpl{Type}?)"/>), 
+        /// 执行移除时也将阻止添加与更新 <br/>
+        /// 暂且不针对键加锁, 这样子够用了
+        /// </remarks>
+        private readonly WorkCountGuard implDicIUGuard = new();
+        private static TimeSpan implDicIUWaitGuardTimeout = TimeSpan.FromSeconds(5);
+        private readonly object compareRemoveLocker = new();
+        public bool TryGetImpl(string name, [NotNullWhen(true)] out IKeyedConfigReadWriteImpl<Type>? impl)
+        {
+            return impls.TryGetValue(name, out impl);
+        }
+        public bool TryAddImpl(string name, IKeyedConfigReadWriteImpl<Type> impl)
+        {
+            using var token = implDicIUGuard.TryBeginWork(implDicIUWaitGuardTimeout);
+            if (!token.GetSuccess) return false;
+
+            return impls.TryAdd(name, impl);
+        }
+        public bool TryUpdateImpl(string name, MaybeNull<IKeyedConfigReadWriteImpl<Type>> comparisonValue, IKeyedConfigReadWriteImpl<Type> impl)
+        {
+            using var token = implDicIUGuard.TryBeginWork(implDicIUWaitGuardTimeout);
+            if (!token.GetSuccess) return false;
+
+            IKeyedConfigReadWriteImpl<Type> comparisonImpl;
+            if (comparisonValue.HasValue) comparisonImpl = comparisonValue.Value;
+            else
+            {
+                if (!impls.TryGetValue(name, out var exist))
+                    return false;
+                else
+                    comparisonImpl = exist;
+            }
+            return impls.TryUpdate(name, impl, comparisonImpl);
+        }
+        public bool TrySetImpl(string name, IKeyedConfigReadWriteImpl<Type> impl)
+        {
+            using var token = implDicIUGuard.TryBeginWork(implDicIUWaitGuardTimeout);
+            if (!token.GetSuccess) return false;
+
+            impls.AddOrUpdate(name, impl, (_, _) => impl);
+            return true;
+        }
+        public bool TryRemoveImpl(string name, [NotNullWhen(true)] out IKeyedConfigReadWriteImpl<Type>? impl)
+        {
+            return impls.TryRemove(name, out impl);
+        }
+
+        public bool TryRemoveImpl(string name, MaybeNull<IKeyedConfigReadWriteImpl<Type>> comparisonImpl, [NotNullWhen(true)] out IKeyedConfigReadWriteImpl<Type>? impl)
+        {
+            if (!comparisonImpl.HasValue) return impls.TryRemove(name, out impl);
+            else
+            {
+                using var token = implDicIUGuard.TryAcquireLock(implDicIUWaitGuardTimeout);
+                if (!token.GetSuccess)
+                {
+                    impl = default;
+                    return false;
+                }
+
+                lock (compareRemoveLocker)
+                {
+                    if (impls.TryGetValue(name, out var exists) && exists == comparisonImpl.Value)
+                    {
+                        if (impls.TryRemove(name, out impl))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                impl = default;
+                return false;
+            }
+        }
+
+
+        public bool TryGetImpl(string name, [NotNullWhen(true)] out IConfigReadWriteImpl? impl)
+        {
+            if (TryGetImpl(name, out IKeyedConfigReadWriteImpl<Type>? exist))
+            {
+                impl = exist;
+                return true;
+            }
+            else
+            {
+                impl = default;
+                return false;
+            }
+        }
+
+        public bool TryAddImpl(string name, IConfigReadWriteImpl impl)
+        {
+            if (!IsAvailable(impl)) return false;
+            else return TryAddImpl(name, (IKeyedConfigReadWriteImpl<Type>)impl);
+        }
+
+        public bool TryUpdateImpl(string name, IConfigReadWriteImpl impl)
+        {
+            if (!IsAvailable(impl)) return false;
+            else return TryUpdateImpl(name, (IKeyedConfigReadWriteImpl<Type>)impl);
+        }
+
+        public bool TrySetImpl(string name, IConfigReadWriteImpl impl)
+        {
+            if (!IsAvailable(impl)) return false;
+            else return TrySetImpl(name, (IKeyedConfigReadWriteImpl<Type>)impl);
+        }
+
+        public bool TryRemoveImpl(string name, MaybeNull<IConfigReadWriteImpl> comparisonImpl, [NotNullWhen(true)] out IConfigReadWriteImpl? impl)
+        {
+            if (comparisonImpl.HasValue)
+            {
+                if (IsAvailable(comparisonImpl.Value) && 
+                    TryRemoveImpl(name, 
+                        new MaybeNull<IKeyedConfigReadWriteImpl<Type>>((IKeyedConfigReadWriteImpl<Type>)comparisonImpl.Value), 
+                        out IKeyedConfigReadWriteImpl<Type>? exist))
+                {
+                    impl = exist;
+                    return true;
+                }
+            }
+            else
+            {
+                if (TryRemoveImpl(name,
+                    MaybeNull<IKeyedConfigReadWriteImpl<Type>>.Null,
+                    out IKeyedConfigReadWriteImpl<Type>? exist))
+                {
+                    impl = exist;
+                    return true;
+                }
+            }
+            impl = default;
+            return false;
+        }
+
+        #endregion
+
+
+        #region 克隆
+        public object Clone(object config)
+        {
+            return cloneObj(config)!;
+        }
+        /// <summary>
+        /// 通过反射 + 属性拷贝实现的对象拷贝
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        private static object? cloneObj(object? obj)
+        {
+            if (obj == null) return null;
+            Type type = obj.GetType();
+            if (type.IsValueType || type == typeof(string))
+            {
+                return obj;
+            }
+            object? output = Activator.CreateInstance(type);
+            if (output == null) return null;
+            PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo property in properties)
+            {
+                if (!property.CanWrite) continue;
+                if (property.PropertyType.IsArray)
+                {
+                    object? arraySourceObj = property.GetValue(obj);
+                    if (arraySourceObj == null) continue;
+                    Array arraySource = (Array)arraySourceObj;
+                    var elementType = property.PropertyType.GetElementType();
+                    if (elementType == null)
+                    {
+                        continue;
+                    }
+                    Array arrayDest = Array.CreateInstance(elementType, arraySource.Length);
+                    for (int i = 0; i < arraySource.Length; i++)
+                    {
+                        object? itemSource = arraySource.GetValue(i);
+                        arrayDest.SetValue(cloneObj(itemSource), i);
+                    }
+                    property.SetValue(output, arrayDest);
+                }
+                else if (property.PropertyType.IsList())
+                {
+                    object? listSourceObj = property.GetValue(obj);
+                    if (listSourceObj == null) continue;
+                    IList listSource = (IList)listSourceObj;
+                    if (Activator.CreateInstance(property.PropertyType) is not IList listDest) continue;
+                    for (int i = 0; i < listSource.Count; i++)
+                    {
+                        object? itemSource = listSource[i];
+                        listDest.Add(cloneObj(itemSource));
+                    }
+                    property.SetValue(output, listDest);
+                }
+                else
+                {
+                    object? value = property.GetValue(obj);
+                    if (value == null) continue;
+                    property.SetValue(output, value);
+                }
+            }
+            return output;
+        }
+        #endregion
+
+
+        #region 缓存
+
+        #region 状态
+        public bool CacheProtecting { get; private set; } = false;
+
+        public void SetCacheProtect(bool b)
+        {
+            CacheProtecting = b;
+        }
+
+        #endregion
+
+        private readonly ConcurrentDictionary<Type, object> caches = [];
+
+        public IEnumerable<Type> AllKeys => caches.Keys;
+
+        public IEnumerable<KeyValuePair<Type, object>> AllCaches => caches;
+
+
+
+        public bool IsLoaded(Type key)
+        {
+            return caches.ContainsKey(key);
+        }
+
+        public bool TryGet(Type key, [NotNullWhen(true)] out object? config)
+        {
+            return caches.TryGetValue(key, out config);
+        }
+
+        public bool TryAdd(Type key, object config)
+        {
+            return caches.TryAdd(key, config);
+        }
+
+        public bool TryUpdate(Type key, object config)
+        {
+            if (CacheProtecting) return false;
+            if (!TryGet(key, out var exist))
+            {
+                return false;
+            }
+            else
+            {
+                if (exist == config) return true;
+                return caches.TryUpdate(key, config, exist);
+            }
+        }
+
+        public bool TryRemove(Type key, [NotNullWhen(true)] out object? config)
+        {
+            if (CacheProtecting)
+            {
+                config = null;
+                return false;
+            }
+            return caches.TryRemove(key, out config);
+        }
+
+        public void ClearCaches()
+        {
+            if (CacheProtecting) return;
+            caches.Clear();
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ Common_Util/Module/Config/IConfigReadWriteImpl.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Module.Config
+{
+    /// <summary>
+    /// 配置读写类的实现
+    /// </summary>
+    public interface IConfigReadWriteImpl
+    {
+    }
+    /// <summary>
+    /// 基于键值加卸载对应配置的配置读写类的实现
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    public interface IKeyedConfigReadWriteImpl<TKey> : IConfigReadWriteImpl
+    {
+        /// <summary>
+        /// 获取输入键值的配置
+        /// </summary>
+        /// <returns></returns>
+        bool TryLoadConfig(TKey key, [NotNullWhen(true)] out object? config);
+        /// <summary>
+        /// 保存输入类型的配置
+        /// </summary>
+        /// <param name="config">配置数据对象</param>
+        bool SaveConfig(TKey key, object config);
+    }
+    /// <summary>
+    /// 以类型作为键值的 <see cref="IKeyedConfigReadWriteImpl{TKey}"/>
+    /// </summary>
+    /// <remarks>
+    /// 键对应的配置对象, 类型与键相同
+    /// </remarks>
+    public interface ITypeKeyedConfigReadWriteImpl : IKeyedConfigReadWriteImpl<Type>
+    {
+        /// <summary>
+        /// 类型是否有效
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        bool IsAvailable(Type type);
+
+        /// <summary>
+        /// 接口默认实现: 获取输入类型的配置, 获取失败时抛出异常
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        T DefaultLoadConfig<T>()
+        {
+            Type type = typeof(T);
+            if (!IsAvailable(type)) throw new InvalidOperationException("目标类型无效")
+            {
+                Data = { ["Type"] = type }
+            };
+            if (TryLoadConfig(type, out var config))
+            {
+                if (config is T tConfig) return tConfig;
+                else 
+                {
+                    throw new InvalidOperationException("加载得到的对象类型与期望类型不同! ")
+                    {
+                        Data = 
+                        {
+                            ["Target"] = type,
+                            ["Loaded"] = config?.GetType()
+                        }
+                    };
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("加载配置失败")
+                {
+                    Data = { ["Type"] = type }
+                };
+            }
+        }
+        /// <summary>
+        /// 接口默认实现: 保存输入类型的配置
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="config">配置数据对象</param>
+        void DefaultSaveConfig<T>(T config)
+        {
+            Type type = typeof(T);
+            if (!IsAvailable(type)) throw new InvalidOperationException("目标类型无效")
+            {
+                Data = { ["Type"] = type }
+            };
+            if (!SaveConfig(type, config!))
+            {
+                throw new InvalidOperationException("保存配置失败")
+                {
+                    Data = { ["Type"] = type }
+                };
+            }
+        }
+    }
+
+}
