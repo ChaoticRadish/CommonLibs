ba97ac58af791b1d1830af810e3f208b882cbf82
ChaoticRadish <UtaYiZzzz@outlook.com>
2025-10-05 21:04:00 +0800

    feat: 调整字符串解析相关内容
    1. 修正 String.Analysis 忽略字符串值的方法的错误
    2. 新增字符序列读取器 CharSequenceReader

---
+++ Common_Util/String/CharSequenceReader.cs
@@ -0,0 +1,505 @@
+﻿using Common_Util.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.String
+{
+    /// <summary>
+    /// 字符序列读取器, 提供一些单向的读取操作
+    /// </summary>
+    public class CharSequenceReader
+    {
+        private readonly IEnumerator<char> enumerator;
+
+        public CharSequenceReader(IEnumerable<char> chars)
+        {
+            enumerator = chars.GetEnumerator();
+            LogicLocation = 0;
+            ReadBuffer = CreateReadBuffer();
+        }
+
+        #region 读取缓存状态
+        /// <summary>
+        /// 当前逻辑读取位置
+        /// </summary>
+        public int LogicLocation { get; private set; }
+        /// <summary>
+        /// 枚举器的当前位置
+        /// </summary>
+        public int EnumeratorIndex { get; private set; }
+        /// <summary>
+        /// 读取缓存
+        /// </summary>
+        private ICharSequenceReadBuffer ReadBuffer { get; init; }
+
+        /// <summary>
+        /// 当前读取缓存的起始位置
+        /// </summary>
+        private int ReadBufferStart { get; set; }
+        /// <summary>
+        /// 当前读取缓存的长度
+        /// </summary>
+        private int ReadBufferCount { get => ReadBuffer.Count; }
+
+        #endregion
+
+        #region 缓存实例
+        /// <summary>
+        /// 创建 <see cref="ReadBuffer"/> 的实例
+        /// </summary>
+        /// <returns></returns>
+        protected virtual ICharSequenceReadBuffer CreateReadBuffer() => new DefaultCharSequenceReadBuffer();
+        #endregion
+
+        #region 提供操作
+        /// <summary>
+        /// 从当前的逻辑位置后 <paramref name="offset"/> 位置处读取一个字符, 但是不推进逻辑位置
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        public bool TryReadValue(int offset, out char output)
+        {
+            output = default;
+            if (offset >= ReadBufferCount)
+            {
+                int needRead = offset + 1 - ReadBufferCount;
+                int readed = ReadToBuffer(needRead);
+                if (needRead > readed) return false;    // 实际读取到的数据量少于需要读取的
+            }
+            output = ReadBuffer[offset];
+            return true;
+        }
+
+        /// <summary>
+        /// 移动 <paramref name="count"/> 个字符, 并忽略他们
+        /// </summary>
+        /// <param name="count"></param>
+        public void Skip(int count)
+        {
+            int needClearBuffer = count >= ReadBufferCount ? ReadBufferCount : count;
+            ReadBuffer.Clear(needClearBuffer);
+            LogicLocation += needClearBuffer;
+            ReadBufferStart += needClearBuffer;
+
+            int needReadDiscard = count - needClearBuffer;
+            for (int i = 0; i < needReadDiscard; i++)
+            {
+                if (!TryReadDiscardFromSource())
+                {
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 从当前位置开始读取, 直到遇到指定的字符串
+        /// </summary>
+        /// <remarks>
+        /// 移动量为 <paramref name="found"/> 的长度, 如果没有找到, 会一直移动到当前读取器源序列的终点
+        /// </remarks>
+        /// <param name="str"></param>
+        /// <param name="found">
+        /// 从当前位置开始到 <paramref name="str"/> 前的字符串 (不包含 <paramref name="str"/> 本身) <br/>
+        /// 当未查找到时, 返回从开始查找的位置, 到源序列结束处的所有字符构成的字符串
+        /// </param>
+        /// <returns>如果读取到末尾都没有找到匹配的字符串, 将返回 <see langword="false"/></returns>
+        public bool TryReadUntil(string str, [NotNullWhen(true)] out string? found)
+        {
+            ArgumentNullException.ThrowIfNull(str, nameof(str));
+            found = null;
+            if (string.IsNullOrEmpty(str)) return false;
+
+            StringBuilder sb = new();
+            int startIndex = LogicLocation;
+            int index = 0;
+            int readCount = 0;
+            while (TryReadSaveWhereLocation(startIndex + index, out char readed))
+            {
+                readCount++;
+                sb.Append(readed);
+
+
+                if (readCount >= str.Length)
+                {
+                    bool foundFlag = true;
+                    for (int checkIndex = 0; checkIndex < str.Length; checkIndex++)
+                    {
+                        if (str[checkIndex] != sb[sb.Length - str.Length + checkIndex])
+                        {
+                            foundFlag = false;
+                            break;
+                        }
+                    }
+                    
+                    // 提交时保留待查找字符串长度的缓存, 在此之前的则不保留
+                    SubmitReaded(startIndex + index + 1 - str.Length - ReadBufferStart);
+
+                    if (foundFlag)
+                    {
+                        sb.Remove(sb.Length - str.Length, str.Length);
+                        found = sb.ToString();
+                        return true;
+                    }
+
+                }
+
+                index++;
+            }
+
+            // 未找到时, 提交当前所有内容
+            SubmitAllBuffer();
+            found = sb.ToString();
+            return false;
+        }
+        /// <summary>
+        /// 从当前位置开始读取, 直到遇到指定的字符串 (忽略字符串值的部分)
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="found">从当前位置开始到 <paramref name="str"/> 前的字符串 (不包含 <paramref name="str"/> 本身)</param>
+        /// <returns></returns>
+        public bool TryReadUntilIgnoreStringText(string str, [NotNullWhen(true)] out string? found)
+        {
+            ArgumentNullException.ThrowIfNull(str, nameof(str));
+            found = null;
+            if (string.IsNullOrEmpty(str)) return false;
+
+            StringBuilder sb = new();
+            int startIndex = LogicLocation;
+            int index = 0;
+            int readCount = 0;
+
+            bool inQuotes = false;
+            const char quoteChar = '"';
+            const char escapeChar = '\\';
+            bool escape = false;
+
+            while (TryReadSaveWhereLocation(startIndex + index, out char readed))
+            {
+                readCount++;
+                sb.Append(readed);
+
+                if (inQuotes)
+                {
+                    switch (readed)
+                    {
+                        case quoteChar:
+                            if (escape)
+                            {
+                                escape = false;
+                            }
+                            else
+                            {
+                                inQuotes = false;
+                            }
+                            break;
+                        case escapeChar:
+                            if (escape)
+                            {
+                                escape = false;
+                            }
+                            else
+                            {
+                                escape = true;
+                            }
+                            break;
+                        default:
+                            if (escape)
+                            {
+                                escape = false;
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (readed)
+                    {
+                        case quoteChar:
+                            inQuotes = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if (readCount >= str.Length)
+                {
+                    bool foundFlag;
+                    if (!inQuotes)
+                    {
+                        foundFlag = true;
+                        for (int checkIndex = 0; checkIndex < str.Length; checkIndex++)
+                        {
+                            if (str[checkIndex] != sb[sb.Length - str.Length + checkIndex])
+                            {
+                                foundFlag = false;
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foundFlag = false;
+                    }
+
+                    // 提交时保留待查找字符串长度的缓存, 在此之前的则不保留
+                    SubmitReaded(startIndex + index + 1 - str.Length - ReadBufferStart);
+
+                    if (foundFlag)
+                    {
+                        sb.Remove(sb.Length - str.Length, str.Length);
+                        found = sb.ToString();
+                        return true;
+                    }
+
+                }
+
+                index++;
+            }
+
+            // 未找到时, 提交当前所有内容
+            SubmitAllBuffer();
+            found = sb.ToString();
+            return false;
+        }
+
+        #endregion
+
+        #region 内部操作
+        /// <summary>
+        /// 尝试从数据源读取一个字符, 读取成功会存入 <see cref="ReadBuffer"/>, 不会推进 <see cref="LogicLocation"/>
+        /// </summary>
+        /// <param name="readed"></param>
+        /// <returns></returns>
+        private bool TryReadSaveFromSource(out char readed)
+        {
+            if (enumerator.MoveNext())
+            {
+                readed = enumerator.Current;
+                ReadBuffer.Add(readed);
+                EnumeratorIndex++;
+                return true;
+            }
+            else
+            {
+                readed = default;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 读取序列位置 <paramref name="location"/> 处的字符
+        /// </summary>
+        /// <remarks>
+        /// 只能读取 <see cref="ReadBufferStart"/> 之后的字符, 如果还不在缓存中, 则将加载到缓存
+        /// </remarks>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        private bool TryReadSaveWhereLocation(int location, out char readed)
+        {
+            readed = default;
+            if (location < ReadBufferStart) return false;
+            if (location >= ReadBufferStart && location < ReadBufferStart + ReadBufferCount)
+            {
+                readed = ReadBuffer[location - ReadBufferStart];
+                return true;
+            }
+            else
+            {
+                int needRead = location + 1 - EnumeratorIndex;
+                int readCount = ReadToBuffer(needRead);
+                if (readCount < needRead)
+                    return false;
+
+                readed = ReadBuffer[location - ReadBufferStart];
+                return true;
+            }
+        }
+        /// <summary>
+        /// 尝试从数据源读取一个字符, 但是不存入 <see cref="ReadBuffer"/>, 会推进 <see cref="LogicLocation"/> (因为做了逻辑操作: 丢弃)
+        /// </summary>
+        /// <remarks>
+        /// 只能在 <see cref="ReadBufferCount"/> = 0 时可用
+        /// </remarks>
+        /// <returns></returns>
+        private bool TryReadDiscardFromSource()
+        {
+            if (ReadBufferCount != 0) return false;
+            if (enumerator.MoveNext())
+            {
+                EnumeratorIndex++;
+                LogicLocation++;
+                ReadBufferStart++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 从数据源读取 <paramref name="length"/> 个字符, 存储到 <see cref="ReadBuffer"/>
+        /// </summary>
+        /// <param name="length"></param>
+        private int ReadToBuffer(int length)
+        {
+            int counter = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (TryReadSaveFromSource(out _))
+                {
+                    counter++;
+                }
+                else break;
+            }
+            return counter;
+        }
+        /// <summary>
+        /// 将已读取的内容提交到逻辑状态
+        /// </summary>
+        /// <param name="length"></param>
+        private void SubmitReaded(int length)
+        {
+            if (length <= 0) return;
+            if (length > ReadBufferCount) throw new InvalidOperationException($"提交读取完成状态失败, 当前缓存字符量 {ReadBufferCount} 小于提交量 {length}");
+            ReadBuffer.Clear(length);
+            LogicLocation += length;
+            ReadBufferStart += length;
+        }
+        /// <summary>
+        /// 将已读取到缓存的内容提交到逻辑状态
+        /// </summary>
+        private void SubmitAllBuffer()
+        {
+            if (LogicLocation == ReadBufferStart)
+            {
+                int length = ReadBufferCount;
+                if (length == 0) return;
+                ReadBuffer.Clear(length);
+                LogicLocation += length;
+                ReadBufferStart += length;
+            }
+            else if (LogicLocation > ReadBufferStart)
+            {
+                int readBufferEnd = ReadBufferStart + ReadBufferCount;
+                ReadBuffer.Clear();
+                LogicLocation = readBufferEnd;
+                ReadBufferStart = readBufferEnd;
+            }
+            else
+            {
+                throw new Exceptions.General.ImpossibleForkException($"逻辑位置不应小于缓存起点");
+            }
+        }
+
+        #endregion
+
+        #region 调试
+        /// <summary>
+        /// 取得描述当前状态的字符串
+        /// </summary>
+        /// <returns></returns>
+        public string GetStatusString(string splitChar = "; ")
+        {
+            return string.Join(splitChar, 
+                $"{nameof(LogicLocation)}: {LogicLocation}", 
+                $"{nameof(EnumeratorIndex)}: {EnumeratorIndex}", 
+                $"{nameof(ReadBufferStart)}: {ReadBufferStart}", 
+                $"{nameof(ReadBufferCount)}: {ReadBufferCount}", 
+                $"{nameof(ReadBuffer)}: {ReadBuffer.GetBufferContent().WhenEmptyDefault("<empty>")}");
+        }
+        #endregion
+    }
+    /// <summary>
+    /// 字符序列读取器的读取缓存接口
+    /// </summary>
+    public interface ICharSequenceReadBuffer
+    {
+        /// <summary>
+        /// 取得特定序号处的索引值
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        char this[int index] { get; }
+
+        /// <summary>
+        /// 缓存数量
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// 添加字符到缓存的末尾
+        /// </summary>
+        /// <param name="c"></param>
+        void Add(char c);
+        /// <summary>
+        /// 添加一系列字符到缓存的末尾
+        /// </summary>
+        /// <param name="chars"></param>
+        void AddRange(IEnumerable<char> chars);
+        /// <summary>
+        /// 从缓存的首部清理 <paramref name="count"/> 个字符
+        /// </summary>
+        /// <param name="count"></param>
+        void Clear(int count);
+        /// <summary>
+        /// 移除所有缓存着的字符
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// 取得缓存内容字符串
+        /// </summary>
+        /// <returns></returns>
+        string GetBufferContent();
+    }
+    public readonly struct DefaultCharSequenceReadBuffer() : ICharSequenceReadBuffer
+    {
+        private readonly Queue<char> buffer = [];
+
+        public char this[int index]
+        {
+            get
+            {
+                ArgumentOutOfRangeException.ThrowIfLessThan(index, 0, nameof(index));
+                ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, buffer.Count, nameof(index));
+                return buffer.Skip(index).Take(1).First();
+            }
+        }
+
+        public int Count => buffer.Count;
+
+        public void Add(char c) => buffer.Enqueue(c);
+
+        public void AddRange(IEnumerable<char> chars)
+        {
+            foreach (var c in chars)
+            {
+                buffer.Enqueue(c);
+            }
+        }
+
+        public void Clear(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                buffer.Dequeue();
+            }
+        }
+        public void Clear()
+        {
+            buffer.Clear();
+        }
+
+        public string GetBufferContent()
+        {
+            return new string(buffer.ToArray());
+        }
+    }
+}
