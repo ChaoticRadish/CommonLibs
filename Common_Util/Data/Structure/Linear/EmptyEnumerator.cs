3f7aef63e50bd4d11efc6f09632eaa0e1de47bbb
ChaoticRadish <UtaYiZzzz@outlook.com>
2025-10-22 23:27:57 +0800

    feat: 完善字符串解析和配置转换功能
    - 配置转换新增Guid支持和更精细的控制。
    - 字符串解析帮助类和字符序列读取器增加了重载方法。
    - 新增EmptyEnumerator和IEnumerable.OffsetOfFirst扩展方法。

---
+++ Common_Util/Data/Structure/Linear/EmptyEnumerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Data.Structure.Linear
+{
+    /// <summary>
+    /// 空的遍历器
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public readonly struct EmptyEnumerator<T> : IEnumerator<T>
+    {
+        public static EmptyEnumerator<T> Shared { get; } = new EmptyEnumerator<T>();
+
+        /// <summary>
+        /// 用于实现 <see cref="IEnumerator{T}"/> 的属性, 返回 <typeparamref name="T"/> 的默认值, 
+        /// 可能出现 <see langword="null"/>
+        /// </summary>
+        public readonly T Current => default!;
+
+        readonly object IEnumerator.Current => Current!;
+
+        public readonly void Dispose() { }
+
+        public readonly bool MoveNext() { return false; }
+
+        public readonly void Reset() { }
+    }
+}
