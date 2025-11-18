5229ca15e638f90c8507134657d6fa9f71d71bb4
ChaoticRadish <UtaYiZzzz@outlook.com>
2025-11-18 16:39:49 +0800

    feat: 新增 null 值感知的内容容器, 以及枚举值转换为描述字符串的转换器

---
+++ Common_Wpf/Converter/EnumDescConverter.cs
@@ -0,0 +1,29 @@
+﻿using Common_Util;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Common_Wpf.Converter
+{
+    /// <summary>
+    /// 调用帮助方法 <see cref="EnumHelper.GetDesc(Enum)"/>, 将传入枚举值转换为字符串
+    /// </summary>
+    [ValueConversion(typeof(Enum), typeof(string))]
+    public class EnumDescConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return EnumHelper.GetDesc((Enum)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
