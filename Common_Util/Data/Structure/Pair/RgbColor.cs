1cc35b3695b006864c54df91dd3cabeec6c42821
ChaoticRadish <UtaYiZzzz@outlook.com>
2025-12-02 16:45:23 +0800

    feat: 完善 RgbaColorB
    1. 底层值改为 uint, 属性改为对 uint 的位修改
    2. 添加与 uint 的隐式互相转换
    3. 添加 JSON 转换器类型, 以 uint 值的形式转换 json, 而不是各个属性的值

--- Common_Util/Data/Structure/Pair/RgbColor.cs
+++ Common_Util/Data/Structure/Pair/RgbColor.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Common_Util.Data.Converter.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Common_Util.Data.Structure.Pair
@@ -9,26 +12,57 @@ namespace Common_Util.Data.Structure.Pair
     /// <summary>
     /// 单字节 RGB 色彩模型
     /// </summary>
+    [JsonConverter(typeof(RgbaColorBJsonConverter))]
     public struct RgbaColorB
     {
+        private uint Data;
+
         /// <summary>
         /// 红色值, 值域 [0, 255)
         /// </summary>
-        public byte R { get; set; }
+        public byte R 
+        {
+            readonly get => (byte)((Data >> 24) & 0xFF); 
+            set => Data = (uint)(value << 24) | (Data & 0x00FFFFFF);
+        }
         /// <summary>
         /// 绿色值, 值域 [0, 255)
         /// </summary>
-        public byte G { get; set; }
+        public byte G
+        {
+            readonly get => (byte)((Data >> 16) & 0xFF);
+            set => Data = (uint)(value << 16) | (Data & 0xFF00FFFF);
+        }
         /// <summary>
         /// 蓝色值, 值域  [0, 255)
         /// </summary>
-        public byte B { get; set; }
+        public byte B
+        {
+            readonly get => (byte)((Data >> 8) & 0xFF);
+            set => Data = (uint)(value << 8) | (Data & 0xFFFF00FF);
+        }
         /// <summary>
         /// 透明度, 值域 [0, 255)
         /// </summary>
-        public byte A { get; set; }
+        public byte A
+        {
+            readonly get => (byte)((Data >> 0) & 0xFF);
+            set => Data = (uint)(value << 0) | (Data & 0xFFFFFF00);
+        }
 
         #region 隐式转换
+        public static implicit operator RgbaColorB(uint color)
+        {
+            return new RgbaColorB()
+            {
+                Data = color
+            };
+        }
+        public static implicit operator uint(RgbaColorB color)
+        {
+            return color.Data;
+        }
+
         public static implicit operator System.Drawing.Color(RgbaColorB colorB)
         {
             return System.Drawing.Color.FromArgb(colorB.A, colorB.R, colorB.G, colorB.B);
@@ -44,6 +78,17 @@ namespace Common_Util.Data.Structure.Pair
             };
         }
         #endregion
+
+        public override string ToString()
+        {
+            return $"R:{R}, G:{G}, B:{B}, A:{A}";
+        }
+    }
+    public class RgbaColorBJsonConverter : StructToUintJsonConverter<RgbaColorB>
+    {
+        public override uint Convert(RgbaColorB obj) => obj;
+
+        public override RgbaColorB Convert(uint value) => value;
     }
 
     /// <summary>
@@ -67,5 +112,10 @@ namespace Common_Util.Data.Structure.Pair
         /// 蓝色值, 值域 [0, 1]
         /// </summary>
         public float A { get; set; }
+
+        public override string ToString()
+        {
+            return $"R:{R}, G:{G}, B:{B}, A:{A}";
+        }
     }
 }
