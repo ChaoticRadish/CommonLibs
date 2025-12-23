9c090a2b3b46efb10ac9728440a00ae7bdab6f17
ChaoticRadish <UtaYiZzzz@outlook.com>
2025-12-23 16:59:09 +0800

    feat: 新增 FlowDocument 的静态转换器并为部分控件添加相关依赖属性
    当前转换的节点中间类型只实现了部分目前用到了的, 转换器只实现了简单场景, 像内嵌表格这样子复杂的尚未实现

---
+++ Common_Wpf/Interfaces/IFlowDocumentConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace Common_Wpf.Interfaces
+{
+    public interface IFlowDocumentConverter
+    {
+        /// <summary>
+        /// 将原始数据转换为流式文档
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        FlowDocument Convert(object? source);
+    }
+
+    public interface IAsyncFlowDocumentConverter : IFlowDocumentConverter
+    {
+        /// <summary>
+        /// 将原始数据异步转换为流式文档
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        ValueTask<FlowDocument> ConvertAsync(object? source, CancellationToken cancellationToken);
+    }
+}
