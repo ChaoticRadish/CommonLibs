64c8fc35a4b4488fa2566d3f3dfaf35b2e25364a
ChaoticRadish <UtaYiZzzz@outlook.com>
2026-01-06 18:54:02 +0800

    feat: 添加 WPF 窗口在 Winform 窗口上 ShowDialog 的扩展方法

---
+++ Common_WpfWinformMix/Wpf/WindowExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_WpfWinformMix.Wpf
+{
+    public static class WindowExtensions
+    {
+        /// <summary>
+        /// 在 Winform 的窗口 <paramref name="winform"/> 上显示 WPF 的模态窗口 <paramref name="window"/> 
+        /// </summary>
+        /// <param name="window"></param>
+        /// <param name="winform">Winform 窗口, 如果是 <see langword="null"/>, 则直接使用 <see cref="System.Windows.Window.ShowDialog"/></param>
+        /// <returns></returns>
+        public static bool? ShowDialog(this System.Windows.Window window, System.Windows.Forms.Form? winform)
+        {
+            if (winform != null)
+            {
+                var helper = new System.Windows.Interop.WindowInteropHelper(window);
+                helper.Owner = winform.Handle;
+            }
+            return window.ShowDialog();
+        }
+    }
+}
