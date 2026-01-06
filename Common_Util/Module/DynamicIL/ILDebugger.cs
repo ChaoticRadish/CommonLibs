12c4f60ae4b95aae5934768b993b9f72f0991891
ChaoticRadish <UtaYiZzzz@outlook.com>
2026-01-06 17:36:52 +0800

    feat: 新增动态生成 IL 的一些帮助类, 元组类型的帮助类, 异步操作协调器
    1. 动态 IL 帮助类: 用于快速插入调试相关 (断点/日志) 的 IL 语句
    2. 元组类型帮助类: 目前提供用于创建具有非常多元素的元组类型的方法
    3. 异步操作协调器: 用于 UI 界面非常常见的操作: 修改值后, 需要实时更新相关的内容 (耗时可能会比较长, 比如访问服务器查询某些东西), 并且修改后的原先未完成的更新需要取消

---
+++ Common_Util/Module/DynamicIL/ILDebugger.cs
@@ -0,0 +1,77 @@
+﻿using Common_Util.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Module.DynamicIL
+{
+    public static class ILDebugger
+    {
+        /// <summary>
+        /// 往 <paramref name="il"/> 添加一段调用 <see cref="System.Diagnostics.Debugger.Break"/> 的语句
+        /// </summary>
+        /// <param name="il"></param>
+        public static void Break(ILGenerator il)
+        {
+            var breakMethod = typeof(System.Diagnostics.Debugger)
+                .GetMethod(nameof(System.Diagnostics.Debugger.Break), BindingFlags.Public | BindingFlags.Static)!;
+            il.Emit(OpCodes.Call, breakMethod);
+        }
+
+        /// <summary>
+        /// 往 <paramref name="il"/> 添加一段调用 <see cref="EnumLogExtensions.Debug{TEnum}"/> 的语句
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="il"></param>
+        /// <param name="enum"></param>
+        /// <param name="message"></param>
+        public static void LogDebug<TEnum>(ILGenerator il, TEnum @enum, string message)
+            where TEnum : Enum
+        {
+            var logMethod = typeof(EnumLogExtensions).GetMethod(nameof(EnumLogExtensions.Debug))!
+                .MakeGenericMethod(typeof(TEnum));
+
+            ILConstantHelper.Load(il, @enum);
+            il.Emit(OpCodes.Ldstr, message);
+            il.Emit(OpCodes.Call, logMethod);
+        }
+        public static void LogDebug<TEnum>(ILGenerator il, TEnum @enum, LocalBuilder local)
+            where TEnum : Enum
+        {
+            if (local.LocalType != typeof(string))
+            {
+                throw new NotSupportedException("暂不支持输出非字符串类型的变量日志");
+            }
+            var logMethod = typeof(EnumLogExtensions).GetMethod(nameof(EnumLogExtensions.Debug))!
+                .MakeGenericMethod(typeof(TEnum));
+
+            ILConstantHelper.Load(il, @enum);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Call, logMethod);
+        }
+
+
+        /// <summary>
+        /// 往 <paramref name="il"/> 添加一段调用 <see cref="EnumLogExtensions.Trace{TEnum}"/> 的语句
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="il"></param>
+        /// <param name="enum"></param>
+        /// <param name="message"></param>
+        public static void LogTrace<TEnum>(ILGenerator il, TEnum @enum, string message)
+            where TEnum : Enum
+        {
+            var logMethod = typeof(EnumLogExtensions).GetMethod(nameof(EnumLogExtensions.Trace))!
+                .MakeGenericMethod(typeof(TEnum));
+
+            ILConstantHelper.Load(il, @enum);
+            il.Emit(OpCodes.Ldstr, message);
+            il.Emit(OpCodes.Call, logMethod);
+        }
+
+    }
+}
---
+++ Common_Util/Module/Reflection/ValueTupleHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Module.Reflection
+{
+    /// <summary>
+    /// 元组相关的帮助类
+    /// </summary>
+    public static class ValueTupleHelper
+    {
+        /// <summary>
+        /// 将输入的类型按顺序组合成元组
+        /// </summary>
+        /// <remarks>
+        /// <paramref name="types"/> 内只有一个类型时, 会返回 <see cref="ValueTuple{T1}"/> 而不是唯一的这个类型
+        /// </remarks>
+        /// <param name="types"></param>
+        /// <returns></returns>
+        public static Type MakeValueTupleType(IEnumerable<Type> types)
+        {
+            Type[] typeArr = types.ToArray();
+            return makeValueTupleType(typeArr);
+        }
+        private static Type makeValueTupleType(ReadOnlySpan<Type> types)
+        {
+            if (types.Length == 0)
+                throw new Common_Util.Exceptions.General.ImplementationException("实现异常, 待整理为元组类型的类型列表为空");
+            if (types.Length <= 7)
+            {
+                Type define = types.Length switch
+                {
+                    1 => typeof(ValueTuple<>),
+                    2 => typeof(ValueTuple<,>),
+                    3 => typeof(ValueTuple<,,>),
+                    4 => typeof(ValueTuple<,,,>),
+                    5 => typeof(ValueTuple<,,,,>),
+                    6 => typeof(ValueTuple<,,,,,>),
+                    7 => typeof(ValueTuple<,,,,,,>),
+                    _ => throw new Common_Util.Exceptions.General.ImpossibleForkException()
+                };
+                return define.MakeGenericType(types.ToArray());
+            }
+            else
+            {
+                // 元组第 8 个元素必须是 TRest, 也就是就算刚好 8 元素, 也需要包裹成 ValueTuple
+                Type define = typeof(ValueTuple<,,,,,,,>);
+                Type[] gArgs = new Type[8];
+                for (int i = 0; i < 7; i++)
+                {
+                    gArgs[i] = types[i];
+                }
+                gArgs[7] = makeValueTupleType(types[7..]);
+                return define.MakeGenericType(gArgs);
+            }
+        }
+
+    }
+}
