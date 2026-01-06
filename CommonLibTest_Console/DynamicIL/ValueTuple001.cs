8901995dabcf6a1dee3f91791f73af1593ff6560
ChaoticRadish <UtaYiZzzz@outlook.com>
2026-01-06 18:55:14 +0800

    test: 异步操作协调器的测试和元组相关的两个测试

---
+++ CommonLibTest_Console/DynamicIL/ValueTuple001.cs
@@ -0,0 +1,30 @@
+﻿using Common_Util.Extensions;
+using Common_Util.Module.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibTest_Console.DynamicIL
+{
+    internal class ValueTuple001() : TestBase("测试反射创建元组")
+    {
+        protected override void RunImpl()
+        {
+            Type type;
+            WritePair(type = typeof(ValueTuple<,>).MakeGenericType(typeof(string), typeof(int)));
+            WritePair(type = typeof(ValueTuple<>).MakeGenericType(typeof(string)));
+            WritePair(type = (1,2,3,4,5,6,7,8).GetType());
+            WritePair(type = (1,2,3,4,5,6,7,8,9,10).GetType());
+
+            List<Type> range = [typeof(string), typeof(int), typeof(byte), typeof(float), typeof(bool)];
+            List<Type> types = new List<Type>();
+            foreach (var i in 20.ForUntil())
+            {
+                types.Add(range.Random());
+                WritePair(ValueTupleHelper.MakeValueTupleType(types));
+            }
+        }
+    }
+}
