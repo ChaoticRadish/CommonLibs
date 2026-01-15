9dbf953696b4f0558a106f468bc7320496ae345e
ChaoticRadish <UtaYiZzzz@outlook.com>
2026-01-15 16:13:26 +0800

    feat: 新增异步指令接口和帮助类, 修正 SimpleCommand 的拼写错误
    帮助类目前只包含一个方法: 检查一个 ICommand 是否包含异步执行方法, 如果有则调用它, 返回其结果 (可以是 Task Task<T> ValueTask ValueTask<T>, 不过目前只测试了 Task 的结果的, 其他的暂未测试), 没有则返回 null

---
+++ Common_Util/Module/Command/IAsyncCommand.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Common_Util.Module.Command
+{
+    public interface IAsyncCommand : ICommand
+    {
+        /// <summary>
+        /// 异步调用指令
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        Task ExecuteAsync(object? parameter);
+    }
+
+    public static class AsyncCommandHelper
+    {
+        /// <summary>
+        /// 尝试以异步方式执行指令
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="parameter"></param>
+        /// <returns>
+        /// 可能的返回值: 
+        /// 1. <see cref="Task"/> 或 <see cref="Task{T}"/> <br/>
+        /// 2. <see cref="ValueTask"/> 或 <see cref="ValueTask{T}"/> <br/>
+        /// 3. <see langword="null"/>: 当不是前两者时返回 <see langword="null"/> <br/>
+        /// </returns>
+        public static object? TryGetExecuteAsyncTask(ICommand command, object? parameter)
+        {
+            if (command == null) return Task.CompletedTask;
+
+            // 本库定义的异步指令
+            if (command is IAsyncCommand asyncCommand)
+            {
+                return asyncCommand.ExecuteAsync(parameter);
+            }
+
+            // 用反射找 ExecuteAsync(object) 方法
+            var method = command.GetType().GetMethod(
+                "ExecuteAsync",
+                BindingFlags.Public | BindingFlags.Instance,
+                null,
+                new[] { typeof(object) },
+                null);
+            if (method != null && (
+                method.ReturnType.IsAssignableTo(typeof(Task))
+                || method.ReturnType == typeof(ValueTask)
+                || TypeHelper.GenericTypeIsDefinitionFrom(method.ReturnType, typeof(ValueTask<>))
+                ))
+            {
+                return method.Invoke(command, [parameter]);
+            }
+            // 用反射找 ExecuteAsync() 方法
+            method = command.GetType().GetMethod(
+                "ExecuteAsync",
+                BindingFlags.Public | BindingFlags.Instance,
+                null,
+                Type.EmptyTypes, 
+                null);
+            if (method != null && (
+                method.ReturnType.IsAssignableTo(typeof(Task))
+                || method.ReturnType == typeof(ValueTask)
+                || TypeHelper.GenericTypeIsDefinitionFrom(method.ReturnType, typeof(ValueTask<>))
+                ))
+            {
+                return method.Invoke(command, null);
+            }
+
+            return null;
+        }
+    }
+}
