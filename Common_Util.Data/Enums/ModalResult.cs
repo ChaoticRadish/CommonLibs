57ea80e8817b4cd57778e039d49eba0605c0169b
ChaoticRadish <UtaYiZzzz@outlook.com>
2026-01-31 14:32:58 +0800

    feat: 添加模态相关的一些类型, 以及定义了 "极性"
    极性: 概念地将实物区分为: 积极 (正向的概念), 消极 (负向的概念), 混沌 (不明确, 无法描述的概念), 三个状态

--- Common_Util.Data/Enums/ModalResult.cs
+++ Common_Util.Data/Enums/ModalResult.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Common_Util.Data.Enums
 {
+    /// <summary>
+    /// 模态结果类型枚举值
+    /// </summary>
     public enum ModalResult
     {
         Custom,
@@ -13,5 +16,25 @@ namespace Common_Util.Data.Enums
         Cancel,
         Yes,
         No,
+    }
+
+    public static class ModalResultExtensions
+    {
+        /// <summary>
+        /// 取得模态结果枚举值对应的默认中文名
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static string DefaultChineseName(this ModalResult result)
+        {
+            return result switch
+            {
+                ModalResult.Ok => "确定",
+                ModalResult.Cancel => "取消",
+                ModalResult.Yes => "是",
+                ModalResult.No => "否",
+                _ => string.Empty,
+            };
+        }
     }
 }
---
+++ Common_Util.Data/Struct/Modal/TagModalResult.cs
@@ -0,0 +1,28 @@
+﻿using Common_Util.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Util.Data.Struct.Modal
+{
+    public interface ITagModalResult
+    {
+        /// <summary>
+        /// 模态结果的类型
+        /// </summary>
+        ModalResult Result { get; }
+        /// <summary>
+        /// 关联标签值
+        /// </summary>
+        object? Tag { get; }
+    }
+
+    public readonly struct TagModalResult : ITagModalResult
+    {
+        public ModalResult Result { get; init; }
+        public object? Tag { get; init; }
+
+    }
+}
