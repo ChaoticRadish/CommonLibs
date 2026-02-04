9d5d840d60e5da2c630bd5426894df5401f8746a
ChaoticRadish <UtaYiZzzz@outlook.com>
2026-02-05 02:10:42 +0800

    feat: 完善 ModalResult 相关内容

--- Common_Util.Data/Enums/ModalResult.cs
+++ Common_Util.Data/Enums/ModalResult.cs
@@ -16,6 +16,14 @@ namespace Common_Util.Data.Enums
         Cancel,
         Yes,
         No,
+        /// <summary>
+        /// 中止
+        /// </summary>
+        Abort,
+        Retry,
+        Ignore,
+        Continue,
+        Chaos,
     }
 
     public static class ModalResultExtensions
@@ -33,7 +41,38 @@ namespace Common_Util.Data.Enums
                 ModalResult.Cancel => "取消",
                 ModalResult.Yes => "是",
                 ModalResult.No => "否",
+                ModalResult.Chaos => "不明确",
+                ModalResult.Abort => "中止",
+                ModalResult.Retry => "重试",
+                ModalResult.Ignore => "忽略",
+                ModalResult.Continue => "继续",
+                ModalResult.Custom => string.Empty,
                 _ => string.Empty,
+            };
+        }
+        /// <summary>
+        /// 取得模态结果枚举值对应的默认极性
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static Polarity DefaultPolarity(this ModalResult result)
+        {
+            return result switch
+            {
+                ModalResult.Ok => Polarity.Positive,
+                ModalResult.Yes => Polarity.Positive,
+                ModalResult.Retry => Polarity.Positive,
+                ModalResult.Continue => Polarity.Positive,
+
+                ModalResult.No => Polarity.Negative,
+                ModalResult.Cancel => Polarity.Negative,
+                ModalResult.Abort => Polarity.Negative,
+                ModalResult.Ignore => Polarity.Negative,
+
+                ModalResult.Chaos => Polarity.Chaos,
+                ModalResult.Custom => Polarity.Chaos,
+
+                _ => Polarity.Chaos,
             };
         }
     }
--- Common_Util.Data/Struct/Modal/TagModalResult.cs
+++ Common_Util.Data/Struct/Modal/TagModalResult.cs
@@ -1,12 +1,16 @@
 ﻿using Common_Util.Data.Enums;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Common_Util.Data.Struct.Modal
 {
+    /// <summary>
+    /// 覆盖标签对象的模态结果
+    /// </summary>
     public interface ITagModalResult
     {
         /// <summary>
@@ -18,11 +22,24 @@ namespace Common_Util.Data.Struct.Modal
         /// </summary>
         object? Tag { get; }
     }
+    /// <summary>
+    /// 标签对象为 <typeparamref name="T"/> 的模态结果
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface ITagModalResult<T> : ITagModalResult
+    {
+        /// <summary>
+        /// 关联标签值
+        /// </summary>
+        new T Tag { get; }
+        object? ITagModalResult.Tag { get => Tag; }
 
+    }
     public readonly struct TagModalResult : ITagModalResult
     {
         public ModalResult Result { get; init; }
         public object? Tag { get; init; }
 
+        public static readonly TagModalResult Ok = new TagModalResult() { Result = ModalResult.Ok, Tag = null };
     }
 }
